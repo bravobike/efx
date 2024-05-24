e5907f30aa2e1036b8446e53dae8e2069d46f5c0
Simon Härer <simon.haerer@bravobike.de>
2024-05-24 10:03:28 +0200

    let children start in test

--- lib/application.ex
+++ lib/application.ex
@@ -5,9 +5,5 @@ defmodule Efx.Application do
     Supervisor.start_link(children(), strategy: :one_for_one)
   end
 
-  if Mix.env() == :test do
-    defp children, do: [EfxCase.MockState]
-  else
-    defp children, do: []
-  end
+  defp children, do: [EfxCase.MockState]
 end
