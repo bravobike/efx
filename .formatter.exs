c10dfb11154721278987bb5cac29bd6b6613a0d8
Simon Härer <simon.haerer@bravobike.de>
2024-08-12 13:24:20 +0200

    fix local without parens in formatting

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,7 @@
+locals_without_parens = [defeffect: 2, delegateeffect: 2]
 # Used by "mix format"
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [defeffect: 2, delegateeffect: 2]
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens]
 ]
